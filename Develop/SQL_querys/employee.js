f35386e3e29c0668d727378ae96456b1bdc97d09
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-05-15 14:51:16 -0400

    updated js around inquirer code and SQL queries

--- Develop/SQL_querys/employee.js
+++ Develop/SQL_querys/employee.js
@@ -2,62 +2,78 @@ const inquirer = require('inquirer');
 const { Pool } = require('pg')
 
 const database = new Pool(
-    {
-        user: 'postgres',
-        password: '1234',
-        host: 'localhost',
-        database: 'company_db'
-    },
-    console.log(`Connected to the company_db database.`)
+  {
+    user: 'postgres',
+    password: '1234',
+    host: 'localhost',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
 )
 
 database.connect();
 
 function GETemployee() {
-    database.query(`SELECT * FROM employee;`, function (err, { rows }) {
-        if (err) {
-            console.log(err);
-        }
-        console.table(rows);
-    });
+  database.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.dept_name as department, r.salary, e1.first_name as manager_first_name, e1.last_name as manager_last_name FROM employee as e JOIN role as r ON e.role_id = r.id JOIN employee as e1 ON e.id = e1.manager_id JOIN department as d ON d.id = r.department_id;`, function (err, { rows }) {
+    if (err) {
+      console.log(err);
+    }
+    console.table(rows);
+  });
 };
 
 function ADDemployee() {
 
-    eQuestions = [
-      {
-        type: 'input',
-        name: 'eFirstName',
-        message: 'What is the employees first name?'
-      },
+  database.query(`SELECT id, first_name, last_name FROM employee`).then(({ rows }) => {
+    eRows = rows.map((item) => ({
+      name: item.first_name,
+      value: item.id
+    }))
 
-      {
-        type: 'input',
-        name: 'eLastName',
-        message: 'What is the employees last name?'
-      },
+    database.query(`SELECT id, title FROM role`).then(({ rows }) => {
+      rRows = rows.map((item) => ({
+        name: item.title,
+        value: item.id
+      }))
 
-      {
-        type: 'input',
-        name: 'eRole',
-        message: 'What is the employees role ID?'
-      },
+      eQuestions = [
+        {
+          type: 'input',
+          name: 'eFirstName',
+          message: 'What is the employees first name?'
+        },
 
-      {
-        type: 'input',
-        name: 'eManager',
-        message: 'Please input the ID of the employees manager.'
-      },
-    ]
-  
-    inquirer
-      .prompt(eQuestions)
-  
-      .then((data) => {
-        database.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ('${data.eFirstName}', '${data.eLastName}', '${data.eRole}', '${data.eManager}');`)
+        {
+          type: 'input',
+          name: 'eLastName',
+          message: 'What is the employees last name?'
+        },
 
-        console.log('Employee succesffully added to the employee table!');
-      });
-      };
+        {
+          type: 'list',
+          name: 'eRole',
+          message: 'What is the employees role?',
+          choices: rRows
+        },
+
+        {
+          type: 'list',
+          name: 'eManager',
+          message: 'Please input the ID of the employees manager.',
+          choices: eRows
+        },
+      ]
+
+      inquirer
+        .prompt(eQuestions)
+
+        .then((data) => {
+          database.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ('${data.eFirstName}', '${data.eLastName}', '${data.eRole}', '${data.eManager}');`)
+
+          console.log('Employee succesffully added to the employee table!');
+        });
+    })
+  })
+};
 
 module.exports = { GETemployee, ADDemployee };
